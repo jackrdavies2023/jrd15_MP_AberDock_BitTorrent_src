0ce5f5d34db26de993639b95345ea1f48b9cc1f5
Jack Ryan Davies <125293787+jackrdavies2023@users.noreply.github.com>
2023-03-21 00:19:15 +0000

    Implemented functional login and logout system.

--- www/include/pages/recover.php
+++ www/include/pages/recover.php
@@ -1,4 +1,10 @@
 <?php
+    // Are we already logged in?
+    if ($login->isLoggedIn()) {
+        header('Location: /');
+        exit();
+    }
+
     $smarty->assign('pageName', 'Recover account');
 
     // Load recover.tpl Smarty template file.
