3e05acfc3bc0e15df19fd3fdbd9f4070272c6d98
Jack Ryan Davies <125293787+jackrdavies2023@users.noreply.github.com>
2023-04-22 10:06:57 +0100

    Enforce "can_viewprofile" permission.

--- www/include/pages/profile.php
+++ www/include/pages/profile.php
@@ -5,7 +5,13 @@
         $account = new Account(db: $db, userIdLong: $userIdLong);
 
         if (count($account->getAccount()) > 0 && $account->getAccount()['is_guest'] == 0) {
-            // Account exists and is not a guest.
+            // Account exists and is not a guest. Do we have permission to view the users profile?
+            if ($login->getAccount()['can_viewprofile'] == 0 &&
+                // We're allowed to view our own profile, even if we don't have the "viewprofile" permission.
+                $userIdLong !== $login->getAccount()['uid_long']
+            ) {
+                throw new Exception("Not permitted to view other profiles!");
+            }
 
             $smarty->assign('viewProfileDetails', $account->getAccount());
         } else {
