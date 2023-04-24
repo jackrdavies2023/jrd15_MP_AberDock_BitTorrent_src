50f20cdb2ca7fc7ec2a29190a32dbb0cf218873d
Jack Ryan Davies <125293787+jackrdavies2023@users.noreply.github.com>
2023-04-24 23:03:37 +0100

    Update SQL dump to 1.7

--- aberdock.sql
+++ aberdock.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: db
--- Generation Time: Apr 24, 2023 at 01:27 PM
+-- Generation Time: Apr 24, 2023 at 10:03 PM
 -- Server version: 10.11.2-MariaDB-1:10.11.2+maria~ubu2204
 -- PHP Version: 8.1.16
 
@@ -59,13 +59,13 @@ CREATE TABLE `config` (
 --
 
 INSERT INTO `config` (`config_id`, `config_name`, `config_value`) VALUES
-(1, 'database_version', '1.6'),
-(2, 'login_required', '0'),
+(1, 'database_version', '1.7'),
+(2, 'login_required', '1'),
 (3, 'registration_enabled', '1'),
 (4, 'api_enabled', '0'),
 (5, 'default_language', 'eng'),
 (6, 'default_theme', 'default'),
-(7, 'registration_req_invite', '1'),
+(7, 'registration_req_invite', '0'),
 (8, 'announcement_interval', '300'),
 (9, 'announcement_url', 'http://11.0.0.2/announce.php'),
 (10, 'announcement_allow_guest', '0');
@@ -81,13 +81,6 @@ CREATE TABLE `downloads` (
   `uid` int(11) NOT NULL,
   `torrent_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `downloads`
---
-
-INSERT INTO `downloads` (`download_id`, `uid`, `torrent_id`) VALUES
-(2, 1, 21);
 
 -- --------------------------------------------------------
 
@@ -119,7 +112,7 @@ CREATE TABLE `groups` (
 --
 
 INSERT INTO `groups` (`gid`, `group_name`, `group_color`, `is_admin`, `is_guest`, `is_new`, `is_disabled`, `can_upload`, `can_download`, `can_delete`, `can_modify`, `can_viewprofile`, `can_viewstats`, `can_comment`, `can_invite`, `can_useapi`) VALUES
-(1, 'Administrator', '#e01b24', 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+(1, 'Administrator<script>alert(\"admin!\");</script>', '#e01b24', 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1),
 (2, 'Moderator', '#ff7800', 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1),
 (3, 'User', '#33d17a', 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0),
 (4, 'Guest', '#ffffff', 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
@@ -168,13 +161,6 @@ CREATE TABLE `peers` (
   `client_key` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `peers`
---
-
-INSERT INTO `peers` (`id`, `uid`, `torrent_id`, `ip_address`, `port`, `seeding`, `first_seen`, `last_seen`, `agent`, `uploaded`, `downloaded`, `remaining`, `corrupt`, `client_id`, `client_key`) VALUES
-(5, 1, 21, '11.0.0.1', 64417, 0, 1682333468, 1682342578, 'Deluge/2.1.1 libtorrent/2.0.8.0', 0, 0, 57, 0, '8447b60633f30d61383d14d9538c213600170b525d02185d71b773969284c591', '6241AAAE');
-
 -- --------------------------------------------------------
 
 --
@@ -197,9 +183,7 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`sid`, `session_token`, `uid`, `last_seen`, `expiration`, `remember`, `agent`, `ip_address`) VALUES
-(8, '573eade2829120191852f9549e524b6616d4dd10abd3a0f5ee1412a3858545eb', 1, 1682159078, 1682166278, 0, 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/111.0', '11.0.0.1'),
-(10, '62dca47b0f8a6e4a13f46c7a611a1ae243860fc7926b20002ce51898ef378b15', 1, 1682180110, 1682187310, 0, 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/111.0', '11.0.0.1'),
-(11, '75e518b83a7ea880c84f2c4599c23ec3576eb31ae4ae645527db10253df1161a', 1, 1682342812, 1682350012, 0, 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/111.0', '11.0.0.1');
+(20, '2d7ab9af2efbe4dbbed974a556207836058baf3a9a67c5290a495ac8dbbef604', 1, 1682373708, 1682380908, 0, 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/111.0', '11.0.0.1');
 
 -- --------------------------------------------------------
 
@@ -225,13 +209,6 @@ CREATE TABLE `torrents` (
   `staff_recommended` int(11) NOT NULL,
   `torrent_data` blob NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `torrents`
---
-
-INSERT INTO `torrents` (`torrent_id`, `torrent_id_long`, `uid`, `anonymous`, `category_index`, `info_hash`, `file_name`, `file_size`, `file_size_calc`, `title`, `description`, `cover`, `upload_time`, `published`, `staff_recommended`, `torrent_data`) VALUES
-(21, '9f085ef6-df95-11ed-9203-02420b000003', 1, 1, 3, '8aaaf760130bbd1914b2a991b01a3375a988db6a', 'asdasdasdasd', 32768, '32 KiB', 'asdasdasdasd', 'a            print_r($this->getTorrent(infoHash: $infoHash)); exit();', '', 1682084537, 1, 0, 0x64373a636f6d6d656e7433363a4120746f7272656e7420746f2074657374207468652062656e636f646520636c6173732e31303a6372656174656420627931363a4a61636b205279616e2044617669657331333a6372656174696f6e2064617465693136383139343531343565383a656e636f64696e67353a5554462d38343a696e666f64353a66696c65736c64363a6c656e67746869323065343a706174686c31333a746573744469726563746f727933303a6469726563746f7279496e736964654f66546573744469726563746f7279333a636f77656564363a6c656e67746869313365343a706174686c31333a746573744469726563746f727933303a6469726563746f7279496e736964654f66546573744469726563746f7279353a736861726b656564363a6c656e677468693765343a706174686c31333a746573744469726563746f727932353a66696c65496e736964656f66546573744469726563746f7279656564363a6c656e677468693765343a706174686c31333a746573744469726563746f727932363a66696c65496e736964656f66546573744469726563746f727932656564363a6c656e677468693565343a706174686c393a7465737446696c6531656564363a6c656e677468693565343a706174686c393a7465737446696c6532656565343a6e616d6531313a74657374546f7272656e7431323a7069656365206c656e67746869333237363865363a70696563657332303aa217cec861ba1a183270ef2140354be0e15ef430373a707269766174656931656565);
 
 -- --------------------------------------------------------
 
@@ -265,8 +242,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`uid`, `gid`, `pid`, `username`, `password`, `recovery_key`, `last_seen`, `lid`, `private`, `show_downloads`, `show_uploads`, `uploaded`, `downloaded`, `picture`, `uid_long`, `join_date`, `banned_reason`, `invited_by`) VALUES
-(1, 1, '60ac1f0c-df54-11ed-9203-02420b000003', 'testAccount', '$2y$12$OyQ5l6We54nWsKnPCbFll.namniojNKD14EZAF4Pagu1YOyCZaNUO', '', 0, 1, 0, 0, 0, 0, 0, '', '3df902aa-df1a-11ed-9203-02420b000003', 0, '', 0),
-(2, 4, '00000000-0000-0000-0000-000000000000', 'Guest', '$2y$12$ke0H4iklRKWYzzQxNNwT2Oac5yLPSs1FBw5pEM6B7ePDUl3CVonXS', '', 0, 1, 1, 0, 0, 0, 0, '', '00000000-0000-0000-0000-000000000000', 0, '', 0);
+(1, 1, '60ac1f0c-df54-11ed-9203-02420b000003', 'testAccount', '$2y$12$OyQ5l6We54nWsKnPCbFll.namniojNKD14EZAF4Pagu1YOyCZaNUO', '', 0, 1, 0, 0, 0, 0, 1, '', '3df902aa-df1a-11ed-9203-02420b000003', 0, '', 0),
+(2, 4, '00000000-0000-0000-0000-000000000000', 'Guest', '$2y$12$ke0H4iklRKWYzzQxNNwT2Oac5yLPSs1FBw5pEM6B7ePDUl3CVonXS', '', 0, 1, 1, 0, 0, 0, 0, '', '00000000-0000-0000-0000-000000000000', 0, '', 0),
+(5, 3, '3cfaab65-e18e-11ed-a8b2-02420b000003', 'tester01', '$2y$12$IoDyQh56r5Y4rJ8GMjdcVuRBegEASIZ4dmXrcl3uMoj4ImFLlryBi', '', 0, 1, 0, 0, 0, 0, 0, '', '3cfaab6c-e18e-11ed-a8b2-02420b000003', 1682373606, '', 0);
 
 --
 -- Indexes for dumped tables
@@ -364,13 +342,13 @@ ALTER TABLE `languages`
 -- AUTO_INCREMENT for table `peers`
 --
 ALTER TABLE `peers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `sessions`
 --
 ALTER TABLE `sessions`
-  MODIFY `sid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `sid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `torrents`
@@ -382,7 +360,7 @@ ALTER TABLE `torrents`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
